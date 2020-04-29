a3e8b2b2a4001328d62509d50c9c55cbed62e8af
Toli <toli@cinch.co.uk>
2020-04-29 23:20:01 +0100

    create app service plan and function app

--- powershell/Create-FunctionApp.ps1
+++ powershell/Create-FunctionApp.ps1
@@ -1,10 +1,10 @@
 
 # temp: remove this
 
-# # create app service plan
-# az appservice plan create --name toli-plan --resource-group toli-rg
-# # create storage account
-# az storage account create -n tolisa -g toli-rg -l ukwest --sku Standard_LRS
+# create app service plan
+az appservice plan create --name toli-plan --resource-group toli-rg
+# create storage account
+az storage account create -n tolisa -g toli-rg -l ukwest --sku Standard_LRS
 
 # create function app
 $output = az functionapp create --name toli --resource-group toli-rg --storage-account tolisa --plan toli-plan --output json | ConvertFrom-Json
