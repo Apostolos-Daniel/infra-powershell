3be45b99c94c6eb42f8ea80a7206bec5a5fdc989
Toli <toli@cinch.co.uk>
2020-04-30 17:10:09 +0100

    set new service connection as azure subscription

--- powershell/Create-FunctionApp.ps1
+++ powershell/Create-FunctionApp.ps1
@@ -1,19 +1,7 @@
-
-# temp: remove this
-
 # create app service plan
-az appservice plan create --name toli-plan --resource-group toli-rg
+az appservice plan create --name toli-plan --resource-group behindthebuzzword
 # create storage account
 az storage account create -n tolisa -g toli-rg -l ukwest --sku Standard_LRS
 
 # create function app
-$output = az functionapp create --name toli --resource-group toli-rg --storage-account tolisa --plan toli-plan --output json | ConvertFrom-Json
-
-$result = $output.properties
-
-Write-Host what we get: $result
-
-# create function
-# func init toli-fa --dotnet
-# func new --template "HTTP trigger" --name FunctionToli --language C#
-# func start --build
+az functionapp create --name toli --resource-group toli-rg --storage-account tolisa --plan toli-plan
