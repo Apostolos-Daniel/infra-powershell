4527a2c8ce198d4658b4c531697e53eb9a36a6fb
Toli <toli@cinch.co.uk>
2020-04-30 17:39:15 +0100

    use behindthebuzzword resource group

--- powershell/Create-FunctionApp.ps1
+++ powershell/Create-FunctionApp.ps1
@@ -1,7 +1,8 @@
+$resourceGroup = "behindthebuzzword"
 # create app service plan
-az appservice plan create --name toli-plan --resource-group behindthebuzzword
+az appservice plan create --name toli-plan --resource-group $resourceGroup
 # create storage account
-az storage account create -n tolisa -g toli-rg -l ukwest --sku Standard_LRS
+az storage account create -n tolisa -g $resourceGroup -l ukwest --sku Standard_LRS
 
 # create function app
-az functionapp create --name toli --resource-group toli-rg --storage-account tolisa --plan toli-plan
+az functionapp create --name toli --resource-group $resourceGroup --storage-account tolisa --plan toli-plan
